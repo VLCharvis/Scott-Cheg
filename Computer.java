be189ce3d092e1c7726dc3a9593ba3ae24a9c681
Tom2656 <thomas.fisher1@ntlworld.com>
2016-02-29 15:46:05 +0000

    Added the backbone for getting AI thread to run
    Needs actual button implementation for testing and further improvements.

--- Computer.java
+++ Computer.java
@@ -8,10 +8,45 @@
 
 package game;
 
-public class Computer extends Player {
+import java.util.Random;
+
+public class Computer extends Player implements Runnable {
+	private boolean m_aiToggled = false;
+	private Board m_board;
 	
-	public Computer(String name) {
-		super(name);
+	public boolean toggleAi() {
+		m_aiToggled = !m_aiToggled;
+		return m_aiToggled;
 	}
 	
+	public Computer(String name, Board board) {
+		super(name);
+		m_board = board;
+	}
+	
+	public void run() {
+		while (true) {
+			while (m_aiToggled) {
+				boolean foundValidMove = false;
+				do {
+					Random rnd = new Random();
+					
+					int row = rnd.nextInt(m_board.getm_Board().size());
+					int column = rnd.nextInt(m_board.getm_Board().get(row).size());
+	
+					if (!m_board.getm_Board().get(row).get(column).m_isMine) {
+						m_board.getm_Board().get(row).get(column).setTileType(true, true);
+					}
+					
+					Tile randomTile = m_board.getm_Board().get(row).get(column);
+				
+					if (!randomTile.isMine()) {
+						foundValidMove = true;
+						m_board.revealTile(row, column);
+					}
+				} while (!foundValidMove);
+			}
+		}
+	}
 }
+
